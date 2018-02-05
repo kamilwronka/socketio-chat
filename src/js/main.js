fd6f5008c64a270e6c0c95a5ea7d28100e9f45d9
Kamil Wronka <kamilwronka7@gmail.com>
2018-02-05 13:58:10 +0100

    Add Login modal

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,31 @@
 (function() {
     'use strict';
     var socket = io();
-    var chatSubmit, chatAppend, sendMsgBtn, message, messages, appendInfo, appendTyping;
-    var nickname = prompt("Podaj swoj nick:");
+    var chatSubmit, chatAppend, sendMsgBtn, message, messages, appendInfo, appendTyping, addUserWindow, logInBtn;
+    var nickname;
     message = document.querySelector('.message-input');
     messages = document.querySelector('.messages');
 
-    socket.emit('user joined', nickname);
+
+
+    addUserWindow = function() {
+       var modal = document.createElement('div');
+       var p = document.createElement('p');
+       var input = document.createElement('input');
+       var btn = document.createElement('button');
+       btn.classList.add('log-in');
+       input.classList.add('log-in-input');
+
+       p.innerHTML = "Podaj swój nick:";
+
+       modal.classList.add('modal');
+       modal.appendChild(p);
+       modal.appendChild(input);
+       modal.appendChild(btn);
+       document.body.appendChild(modal);
+    };
+
+    addUserWindow();
 
     chatSubmit = function() {
         var getDate = new Date(),
@@ -36,8 +55,19 @@
     };
     appendTyping = function(nickname) {
         var elem = document.createElement('div');
+        var div = document.createElement('div');
+        div.classList.add('wave');
+        var span = [];
+
+        for(var i = 0; i < 3; i++) {
+            span[i] = document.createElement('span');
+            span[i].innerHTML = ".";
+            span[i].classList.add('dot');
+            div.appendChild(span[i]);
+        }
         elem.classList.add('message');
-        elem.innerHTML = nickname + " is typing...";
+        elem.innerHTML = nickname + " is typing";
+        elem.appendChild(div);
         messages.appendChild(elem);
     };
     appendInfo = function(info) {
@@ -55,6 +85,7 @@
     socket.on('user typing', function(nickname, state) {
         if(state === 1) {
             appendTyping(nickname);
+            state = 0;
         } else if(state === 0) {
 
         }
@@ -63,8 +94,17 @@
         appendInfo(nickname + " disconnected");
     });
 
+    logInBtn = document.querySelector('log-in');
+    logInBtn.addEventListener('click', function() {
+        var logInInput = document.querySelector('log-in-input');
+        nickname = logInInput.value;
+        socket.emit('user joined', nickname);
+    });
+
     sendMsgBtn = document.querySelector('.send-message-btn');
     sendMsgBtn.addEventListener('click', chatSubmit);
+
+
 
     document.body.addEventListener('keydown', function(ev) {
         console.log(ev.keyCode);
@@ -73,10 +113,10 @@
         } else if(message === document.activeElement && ev.keyCode === 13) {
             chatSubmit();
         }
-       // if(message.value.length > 0) {
-       //     var state = 1;
-       //     socket.emit('user typing', nickname, state);
-       //     console.log(message.value);
-       // }
+       if(message.value.length > 0 && message.value.length < 2) {
+           var state = 1;
+           socket.emit('user typing', nickname, state);
+            console.log(message.value);
+        }
     })
 })();
