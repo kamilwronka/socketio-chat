7d4470ee58a45d1c747b7369916322eb84865fea
Kamil Wronka <kamilwronka7@gmail.com>
2018-02-17 12:58:23 +0100

    Fixes

--- index.js
+++ index.js
@@ -15,17 +15,6 @@ io.on('connection', function(socket){
     socket.on('user joined', function(nickname) {
         console.log(nickname + " joined chat");
         io.emit('user joined', nickname);
-<<<<<<< HEAD
-        socket.on('disconnect', function(){
-            console.log(nickname +  ' disconnected');
-            io.emit('disconnect', nickname);
-        });
-    });
-    socket.on('user typing', function(nickname) {
-
-       io.emit('user typing', nickname);
-    });
-=======
         users.push(nickname);
         io.emit('active users', users.length, users);
         console.log(users + "," + users.length);
@@ -43,7 +32,6 @@ io.on('connection', function(socket){
 
        io.emit('user typing', nickname, typing);
     });
->>>>>>> 5537612271980240e8692e8e1fe0ca3904b0e1fb
     socket.on('chat message', function(msg, date, nickname) {
         console.log(date + " " + nickname + " - " + msg);
         io.emit('chat message', msg, date, nickname);
